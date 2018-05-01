a8e45e22f639838c715aff29ad35c713be64719f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-01 20:04:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ function init() {
 
 
 Handlebars.registerHelper('display_ingredient', function() {
-  if(this.state === "closed") {
-    return new Handlebars.SafeString(this.body)
-  } else {
-    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
-  }
+display each ingredient within the each block
+
 })
 
 document.addEventListener("DOMContentLoaded", function(event) {
