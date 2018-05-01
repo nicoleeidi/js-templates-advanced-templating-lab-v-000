d3259897c3185d6fb56e18b3a5cd61888e6e0abe
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-01 20:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 })
 Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
-function createRecipe(){
-  
-}
-function updateRecipe(){
-  
-}
+
+ function createRecipe() {
+   var recipe = getRecipeVals()
+   var recipeTemplate = document.getElementById("recipe-template").innerHTML
+   var template = Handlebars.compile(recipeTemplate)
+   document.getElementById("main").innerHTML = template(recipe)
+ }
+
+ function updateRecipe() {
+   var recipe = getRecipeVals()
+   var recipeTemplate = document.getElementById("recipe-template").innerHTML
+   var template = Handlebars.compile(recipeTemplate)
+   document.getElementById("main").innerHTML = template(recipe)
+ }
