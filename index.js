ffe95f349a774bc2a876f0a13e297365a2036763
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-01 20:00:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function init() {
   //put any page initialization/handlebars initialization here
 }
+
+
+Handlebars.registerHelper('display_ingredient', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
