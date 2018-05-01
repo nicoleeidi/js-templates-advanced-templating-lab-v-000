4da52cb2c493d98eee3d0067c8e20c46407ae966
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-01 20:09:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function init() {
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+ var template = Handlebars.compile(formTemplate)
+ document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
   //put any page initialization/handlebars initialization here
 }
 
